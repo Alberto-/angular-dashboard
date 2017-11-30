764f65c08a3c3dc07ad5b6f8508e757de973ddd9
Alberto Piras <alberto.piras@inpeco.com>
2017-11-30 14:56:31 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
-let cleanCSS = require('gulp-clean-css');
 
-
-gulp.task('compress',['minify-css'], function() {
-  gulp.src(['./assets/js/*.js'])
-    .pipe(concat('main.js'))
-    .pipe(minify({noSource :true}))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('./assets/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('listen', ['compress'], function () {
-    // watch for JS changes
-    gulp.watch('js/*.js', function () {
-        // run compress task
-        gulp.run('compress');
-    });
+gulp.task('compress', function () {
+    gulp.src(['./app/widgets/**/*Service.js',
+        './app/widgets/**/**/*Service.js',
+        './app/widgets/**/*Controller.js',
+        './app/widgets/**/**/*Controller.js',
+        './app/widgets/**/*Directive.js',
+        './app/widgets/**/**/*Directive.js',
+        './app/widgets/**/*Filter.js',
+        './app/widgets/**/**/*Filter.js'
+    ])
+        .pipe(concat('main.js'))
+        .pipe(minify({ noSource: true }))
+        .pipe(gulp.dest('./app/dist/'));
 });
 
