f6a3b81a901057f31c2f23a9ad5672d5752356d8
Alberto Piras <alberto.piras@inpeco.com>
2017-11-30 14:55:59 +0100

    Add a minified file for widgets

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('dashboardApp',['ngRoute','ui.sortable','chart.js']);
+var app = angular.module('dashboardApp', ['ngRoute', 'ui.sortable', 'chart.js']);
 
 
 app.config(function ($httpProvider) {
@@ -7,73 +7,79 @@ app.config(function ($httpProvider) {
 
 app.controller('mainController', function ($scope, $rootScope, $http, $compile) {
 
-  $scope.dashboardAddress =  dashboard.dashboardHost;
+  $scope.dashboardAddress = dashboard.dashboardHost;
 
-// if is not a mobile device enable draggable interface
-if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )) {
+  // if is not a mobile device enable draggable interface
+  if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
 
-  console.info("NOT mobile: activating draggable..");
+    console.info("NOT mobile: activating draggable..");
 
-  // angular.element(document.getElementById('mainController')).scope().makeDraggable();
+    // angular.element(document.getElementById('mainController')).scope().makeDraggable();
 
-  $scope.sortableOptions = {
-    connectWith: ".draggable",
-    handle: ".move, .portlet-header",
-    cancel: ".portlet-toggle",
-    placeholder: "portlet-placeholder ui-corner-all",
-    cursor: "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAA3lBMVEUAAAAnk8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8J5sPN9AAAASXRSTlMAAQIDBAUGBwgODxESFBUZGxwfIywuMTQ2ODk7PD1AQ0dQVFVWWVxfYWNka21zdYOGjo+XmqWmq7Cyt7nAwcPI0dri6O3z9/n718TKMwAAALxJREFUGBl1wddWwkAARdEzKYJYKCo2jJSAvYuiqKCGCff/fwgWEBwf2JvVPD9s3zc2ouOQjPcgpfZ9LCnZLRpmbtU5sZuUPv2SpO8CkI+SAdwA5zAs5HpfsDWSelADahDBgXxeP4L1Lo6qQmwbLnFUFaIYznDsK0AxHOFoCX6u+a/bhytbxGXSDpg3W8axrQrgvaQV/tTlMWWexzssPfWZMY82IPN7wZyvQxbyili401KyRmavGc+d5lhhAtNfGTQkXlpjAAAAAElFTkSuQmCC'), pointer",
-    cursorAt: { left : 200 },
-    receive: function( event, ui ) {
-      console.debug(event);
-      console.debug(ui);
-      var listaId=this.id;
-      var cardId=ui.item[0].id;
-      console.debug("-->update to column :"+listaId+"\n Element id: "+cardId);
-      reloadChart();
-  //$scope.moveCard(cardId,listaId);
-},
-start: function( event, ui ) {
-   angular.element(".draggable").addClass("draggable_min_height");
-       // $(".draggable").addClass("showBordersMobile");
-      // $("#showBorders").prop('checked', true);
-      reloadChart();
-    },
-    stop: function( event, ui ) {
-      angular.element(".draggable").removeClass("draggable_min_height");
-       // $(".draggable").removeClass("showBordersMobile");
-       //  $("#showBorders").prop('checked', false);
-       reloadChart();
-       window.dispatchEvent(changeDashboard);
-     }
-    /** OTHER CONTROLS ON START AND STOP DRAGGING
-    start: function( event, ui ) {
-      console.log(event.target);
-      angular.element(this).addClass('dragging-element');
-    },
-    stop: function( event, ui ) {
-      console.log(event.target);
-       angular.element(this).removeClass('dragging-element');
-     },***/
-   };
+    $scope.sortableOptions = {
+      connectWith: ".draggable",
+      handle: ".move, .portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all",
+      cursor: "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAA3lBMVEUAAAAnk8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8Ink8J5sPN9AAAASXRSTlMAAQIDBAUGBwgODxESFBUZGxwfIywuMTQ2ODk7PD1AQ0dQVFVWWVxfYWNka21zdYOGjo+XmqWmq7Cyt7nAwcPI0dri6O3z9/n718TKMwAAALxJREFUGBl1wddWwkAARdEzKYJYKCo2jJSAvYuiqKCGCff/fwgWEBwf2JvVPD9s3zc2ouOQjPcgpfZ9LCnZLRpmbtU5sZuUPv2SpO8CkI+SAdwA5zAs5HpfsDWSelADahDBgXxeP4L1Lo6qQmwbLnFUFaIYznDsK0AxHOFoCX6u+a/bhytbxGXSDpg3W8axrQrgvaQV/tTlMWWexzssPfWZMY82IPN7wZyvQxbyili401KyRmavGc+d5lhhAtNfGTQkXlpjAAAAAElFTkSuQmCC'), pointer",
+      cursorAt: { left: 200 },
+      receive: function (event, ui) {
+        console.debug(event);
+        console.debug(ui);
+        var listaId = this.id;
+        var cardId = ui.item[0].id;
+        console.debug("-->update to column :" + listaId + "\n Element id: " + cardId);
+        reloadChart();
+        //$scope.moveCard(cardId,listaId);
+      },
+      start: function (event, ui) {
+        angular.element(".draggable").addClass("draggable_min_height");
+        // $(".draggable").addClass("showBordersMobile");
+        // $("#showBorders").prop('checked', true);
+        reloadChart();
+      },
+      stop: function (event, ui) {
+        angular.element(".draggable").removeClass("draggable_min_height");
+        // $(".draggable").removeClass("showBordersMobile");
+        //  $("#showBorders").prop('checked', false);
+        reloadChart();
+        window.dispatchEvent(changeDashboard);
+      }
+      /** OTHER CONTROLS ON START AND STOP DRAGGING
+      start: function( event, ui ) {
+        console.log(event.target);
+        angular.element(this).addClass('dragging-element');
+      },
+      stop: function( event, ui ) {
+        console.log(event.target);
+         angular.element(this).removeClass('dragging-element');
+       },***/
+    };
 
- }else{
-  $scope.sortableOptions = {
-    disabled: true
+  } else {
+    $scope.sortableOptions = {
+      disabled: true
+    }
   }
-}
 
- angular.element("#showBordersButton").click(function () {
-  console.info('Show/hide template ...')
-   angular.element(".draggable").toggleClass("showBorders");
- });
+  angular.element("#showBordersButton").click(function () {
+    console.info('Show/hide template ...')
+    angular.element(".draggable").toggleClass("showBorders");
+  });
 
+});
+
+// GRAVATAR
+$(document).ready(function () {
+  var hash = CryptoJS.MD5("albe45313@gmail.com");
+  document.getElementById("authorPhoto").src = "http://www.gravatar.com/avatar/" + hash;
 });
 
 var changeDashboard = document.createEvent('Event');
 changeDashboard.initEvent('changeDashboard', true, true);
 
-function reloadChart(){
-   angular.element(window).trigger('resize');
+function reloadChart() {
+  angular.element(window).trigger('resize');
 
   if (document.createEvent) { // W3C
     var ev = document.createEvent('Event');
@@ -82,10 +88,9 @@ function reloadChart(){
     window.dispatchEvent(ev);
   }
   else { // IE
-    element=document.documentElement;
-    var event=document.createEventObject();
-    element.fireEvent("onresize",event);
+    element = document.documentElement;
+    var event = document.createEventObject();
+    element.fireEvent("onresize", event);
   }
 };
-
 
