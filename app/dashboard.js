ceb0d2d6bb2b1b6ada5ec61f8944c333df8f496f
Alberto Piras <alberto.piras@inpeco.com>
2017-12-06 11:48:42 +0100

    minor refactor

--- app/dashboard.js
+++ app/dashboard.js
@@ -2,7 +2,9 @@
   -dashboard utilities object-
 
   You can insert here the other general stuff you need
-  to use globally in the dashboard
+  to use globally in the dashboard.
+
+  At the bottom you can find the APIKEY to change in your app.
 
 */
 
@@ -24,7 +26,7 @@
 })(this);
 
 /**You need a GOOGLE API Key - this will work only on the demo site*/
-var GMAPS_APIKEY = "AIzaSyA8U6jPDUV3vrsIWbVvSXnfMh0aOJT2I2Usensor";
+var GMAPS_APIKEY = "AIzaSyA8U6jPDUV3vrsIWbVvSXnfMh0aOJT2I2U";
 
 /**You need an API KEY from http://openweathermap.org/api (it's FREE) - this will work only on the demo site*/
 var API_KEY_OPEN_WEATHER = "528b737fd7f101e5f15733bb9926b0bc"; 
